6cded01b076c94f5eb6adc3ef53d15bee2ddba6e
Mhef-Technology <mhef.technology@gmail.com>
2023-11-10 10:18:40 -0600

    Modificado

--- src/java/conexión/conectadita.java
+++ src/java/conexión/conectadita.java
@@ -6,7 +6,7 @@ public class conectadita {
     public Connection conectar () {
         try{
             Class.forName( "com.mysql.cj.jdbc.Driver");
-            cnx = DriverManager.getConnection ("jdbc:mysql://localhost:3308/Meksh?autoReconnect=true&useSSL=false",  "root",  "n0m3l0");
+            cnx = DriverManager.getConnection ("jdbc:mysql://localhost:3306/Meksh?autoReconnect=true&useSSL=false",  "root",  "n0m3l0");
         }
         catch (ClassNotFoundException | SQLException error) {
             out.print("<script>alert('Error al conectar con la base de datos')</script>");
