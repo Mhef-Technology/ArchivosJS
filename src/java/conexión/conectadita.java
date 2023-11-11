8cb12136dba3313e027e38c64e6812d152c6177b
Fersnick <jimenez.alvarez.josefernando@gmail.com>
2023-11-10 19:34:29 -0600

    Bien

--- src/java/conexión/conectadita.java
+++ src/java/conexión/conectadita.java
@@ -1,16 +1,60 @@
 package conexión;
+
+import java.io.Serializable;
 import java.sql.*;
-import static java.lang.System.out;
-public class conectadita {
-    Connection cnx = null;
-    public Connection conectar () {
-        try{
-            Class.forName( "com.mysql.cj.jdbc.Driver");
-            cnx = DriverManager.getConnection ("jdbc:mysql://localhost:3306/Meksh?autoReconnect=true&useSSL=false",  "root",  "n0m3l0");
+
+public class conectadita implements Serializable{
+
+    public static String user = "root";
+    public static String password = "1234";
+    public static String db = "Bitcube";
+    public static String port = "3306";
+
+    public boolean testDriver() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        catch (ClassNotFoundException | SQLException error) {
-            out.print("<script>alert('Error al conectar con la base de datos')</script>");
+        return false;
+    }
+
+    public Connection getConnection(String user, String password, String db, String server) {
+        String url = null;
+        if (user == null || password == null || db == null || server == null) {
+            return null;
         }
-        return cnx;
+        if ("".equals(user) || "".equals(password) || "".equals(db) || "".equals(server)) {
+            return null;
+        }
+        url = String.format("jdbc:mysql://%s:%s/%s?user=%s&password=%s", server, port, db, user, password);
+        try {
+            if (!testDriver()) {
+                return null;
+            }
+            return DriverManager.getConnection(url);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public Connection getConnection() {
+        return getConnection(user, password, db, "localhost");
+    }
+
+    public void closeConnection(Connection connection) {
+        try {
+            if (connection == null) {
+                return;
+            }
+            if (connection.isClosed()) {
+                return;
+            }
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
